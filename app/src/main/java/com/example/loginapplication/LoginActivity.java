b2eea6335f5970b75b22fbeecc7b1a501e3e8281
Aman <harshit.tiwari2020@vitbhopal.ac.in>
2022-08-06 13:07:15 +0530

    first commit

---
+++ app/src/main/java/com/example/loginapplication/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+}
---
+++ app/src/main/java/com/example/loginapplication/Splashscreen.java
@@ -0,0 +1,24 @@
+package com.example.loginapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen.this, LoginActivity.class));
+            }
+        },2000);
+    }
+}
