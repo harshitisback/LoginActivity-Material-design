e2288274732c57d00d9834a331e94cbfe17f30b3
Aman <harshit.tiwari2020@vitbhopal.ac.in>
2022-08-07 10:10:20 +0530

    email auth complete

--- app/src/main/java/com/example/loginapplication/SignUp.java
+++ app/src/main/java/com/example/loginapplication/SignUp.java
@@ -1,15 +1,23 @@
 package com.example.loginapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
@@ -18,6 +26,7 @@ public class SignUp extends AppCompatActivity {
     Button signup;
 
     TextInputLayout name, phone, email, password;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,7 @@ public class SignUp extends AppCompatActivity {
         email = (TextInputLayout)findViewById(R.id.email);
         phone = (TextInputLayout)findViewById(R.id.phone);
         password = (TextInputLayout)findViewById(R.id.password);
+        progressBar = (ProgressBar)findViewById(R.id.pb);
 
 
 
@@ -70,6 +80,11 @@ public class SignUp extends AppCompatActivity {
             password.requestFocus();
             return;
         }
+        if (Phone.isEmpty()){
+            phone.setError("Your Phone is required");
+            phone.requestFocus();
+            return;
+        }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
             email.setError("Please Provide Valid email");
@@ -81,6 +96,34 @@ public class SignUp extends AppCompatActivity {
             password.requestFocus();
             return;
         }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(Name,Email,Phone);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                progressBar.setVisibility(View.GONE);
+                                                Toast.makeText(SignUp.this,"User has been registered succesfully",Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(SignUp.this,LoginActivity.class));
+                                            }else {
+                                                Toast.makeText(SignUp.this,"Registration Failed",Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+
+                        }else{
+                            Toast.makeText(SignUp.this,"Registraion Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
     }
